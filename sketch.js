58b1808d6eeaa6a4139a2a42a9baa760222281ef
Danielle Tice <danielletice@Danielles-MacBook-Pro.local>
2020-09-10 14:05:40 -0400

    made a bird and a moving circle

--- sketch.js
+++ sketch.js
@@ -1,8 +1,44 @@
+let xpos=0
+let ypos=0
 function setup() {
   // put setup code here
-  background(101);
+createCanvas(750,750);
+background(0,100,200);
+
 }
 
+
+
+
 function draw() {
-  // put drawing code here
+  // put drawing code he
+
+noStroke()
+fill(50)
+triangle(250,375,150,50,350,375);
+
+noStroke()
+fill(70)
+ellipse(350,400,220,300);
+
+noStroke()
+fill(70)
+ellipse(350,200,190,190);
+
+noStroke()
+fill(85)
+triangle(375,310,420,475,650,100);
+
+noStroke()
+fill(70)
+triangle(200, 275, 258, 220, 300, 275);
+
+fill(200)
+circle(325,200,30);
+
+circle(xpos,ypos,50)
+xpos= xpos+2
+ypos= ypos+1;
+
+
 }
