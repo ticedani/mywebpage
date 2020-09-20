86e58da441c556b19d10ec06f3f1f957b532e2df
Danielle Tice <danielletice@Danielles-MacBook-Pro.local>
2020-09-19 23:55:34 -0400

    move ball and click on it randomly moves

--- in classassignment 2/sketch.js
+++ in classassignment 2/sketch.js
@@ -1,15 +1,44 @@
 
+let xpos = 200
+let ypos = 0
+let xballspeed = 7
+let yballspeed = 5
+random(0,10000)
+
 function setup() {
   // put setup code here
-createCanvas(200,200);
-background(200,10,120);
-
+createCanvas(windowWidth, windowHeight);
 }
 
 function draw() {
   //Put draw code here
-  fill(30,40,200);
-  circle(100,100,70);
+  background(20,20,20);
+  fill(mouseX, mouseY,200);
+  circle(xpos,ypos ,100);
 
+  xpos = xpos + xballspeed
+  ypos = ypos + yballspeed
 
+//
+  if(xpos > width){
+  xballspeed = -xballspeed
+  }
+
+  if (xpos < 0){
+    xballspeed = -xballspeed
+  }
+
+  if(ypos > windowHeight){
+    yballspeed = -yballspeed
+  }
+
+  if(ypos < 0){
+    yballspeed = -yballspeed
+  }
+
+if (mouseIsPressed & dist (mouseX, mouseY,xpos ,ypos) < 70){
+xpos= random(0,windowWidth)
+ypos= random(0,windowHeight)
+
+}
 }
--- sketch.js
+++ sketch.js
@@ -1,15 +1,42 @@
-
+let xpos=0
+let ypos=0
 function setup() {
   // put setup code here
-createCanvas(700,700);
-background(200,10,120);
+createCanvas(750,750);
+background(0,100,201);
 
 }
 
+
+
 function draw() {
-  fill(30,40,200);
-  circle(20,20,70);
-  fill(200,40,200);
-  rect(400,400,100,50);
+  // put drawing code he
+
+noStroke()
+fill(50)
+triangle(250,375,150,50,350,375);
+
+noStroke()
+fill(70)
+ellipse(350,400,220,300);
+
+noStroke()
+fill(70)
+ellipse(350,200,190,190);
+
+noStroke()
+fill(85)
+triangle(375,310,420,475,650,100);
+
+noStroke()
+fill(70)
+triangle(200, 275, 258, 220, 300, 275);
+
+fill(200)
+circle(325,200,30);
+
+circle(mouseX,mouseY,50)
+
+
 
 }
